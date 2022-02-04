c43d5de5c8100485df91527ac1403509ed14c130
pavelloi <pashaloy95@gmail.com>
2022-02-04 22:12:50 +0200

    1st task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
 
+const onStartClick = () => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.setAttribute("disabled", "disabled")
+};
+
+const onStopClick = () => {
+    clearInterval(timerId);
+    startBtn.removeAttribute("disabled", "disabled");
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+startBtn.addEventListener("click", onStartClick);
+stopBtn.addEventListener("click", onStopClick);
