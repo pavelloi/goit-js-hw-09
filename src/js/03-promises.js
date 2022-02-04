45c7cce40bf0f84760e34318ac041244e5083242
pavelloi <pashaloy95@gmail.com>
2022-02-05 01:22:44 +0200

    task 3 in process

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
+import Notiflix from 'notiflix';
+
+const DELAY = document.querySelector("input[name = delay]");
+const delayStep = document.querySelector("input[name = step]");
+const delayQuantity = document.querySelector("input[name = amount]");
+const startPromiseBtn = document.querySelector("button");
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  delayQuantity = position;
+  position += 1;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`Fullfilled promise`);
+      }
+      reject('❌ Упс, у нас закончились продукты');
+    }, delay);
+  })
+  
 }
+
+startPromiseBtn.addEventListener("click", createPromise);
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
